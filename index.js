9648b07c91cdb89faf1fec86640ace920871b033
samaromm <samaromm98@gmail.com>
2020-03-21 22:22:46 +0300

    fixing errors

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let btn = document.querySelectorAll('button')
 
+let input= document.querySelectorAll("input");
+let getDisplay = document.getElementById("display");
+for(let i=0; i<input.length; i++){
+  input[i].addEventListener("input",()=>{
+     let red = document.getElementById("red").value,
+     green = document.getElementById("green").value,
+     blue = document.getElementById("blue").value;
+     getDisplay.style.backgroundColor=`rgb(${red},${green},${blue})`;
+  });
+}
+
+
+let btn = document.querySelectorAll('button')
 for(let i=0; i<btn.length;i++){
-    btn[i].addEventListener('click',function(){
-        let red=document.getElementById('red').value
-        let green=document.getElementById('green').value
-        let blue=document.getElementById('blue').value
-        let header=document.getEle
+    btn[i].addEventListener('click',()=>{
+        let red = document.getElementById("red").value,
+        green = document.getElementById("green").value,
+        blue = document.getElementById("blue").value;
         let toChange=document.querySelector(`${btn[i].value}`)
         toChange.style.backgroundColor=`rgb(${red},${green},${blue})`
     })
